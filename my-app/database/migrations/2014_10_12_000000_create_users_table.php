7583827c74faa4f68338e59e45c5c0fcec1bfcfc
ryotamatsumoto3995 <sasadangonrm@gmail.com>
2023-12-07 17:10:02 +0900

    [add] ユーザーモデルのマイグレーションを実行

--- my-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ my-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->default('new_user');
             $table->string('email')->unique();
+            $table->integer('age')->nullable();
+            $table->enum('gender', ['male', 'female', 'other', 'private'])->default('private');
+            $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
